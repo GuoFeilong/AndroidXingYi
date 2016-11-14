fd4608cf70e2740ac154903496a0586fc1dba5b3
Jsion <guofeilongmr@qq.com>
2016-11-14 15:05:49 +0800

    base fragment加入activity值以及防止错乱代码,加入bundle和activity正确的传参
    base fragment加入activity值以及防止错乱代码,加入bundle和activity正确的传参

--- app/src/main/java/com/android/xingyi/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/android/xingyi/ui/fragment/BaseFragment.java
@@ -2,8 +2,10 @@ package com.android.xingyi.ui.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +15,39 @@ import android.view.ViewGroup;
  * Created by Feilong.Guo on 2016/11/13.
  */
 
-// TODO: 2016/11/13 完善生命周期的处理以及bundle传参
-public abstract class BaseFragment extends Fragment {
+public class BaseFragment extends Fragment {
+    private static final String STATE_SAVE_IS_HIDDEN = "STATE_SAVE_IS_HIDDEN";
+    protected static final String BUNDLE_FRAGMENT_KEY = "bundle_fragment_key";
+    protected Context activityContext;
+    protected Parcelable bundleParcel;
+    public BaseFragment() {
+    }
+
+    public static BaseFragment newInstance(Parcelable parcelable) {
+        BaseFragment fragment = new BaseFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(BUNDLE_FRAGMENT_KEY, parcelable);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            boolean isSupportHidden = savedInstanceState.getBoolean(STATE_SAVE_IS_HIDDEN);
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            if (isSupportHidden) {
+                ft.hide(this);
+            } else {
+                ft.show(this);
+            }
+            ft.commit();
+        }
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            bundleParcel = arguments.getParcelable(BUNDLE_FRAGMENT_KEY);
+        }
     }
 
     @Nullable
@@ -29,5 +59,17 @@ public abstract class BaseFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        this.activityContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_SAVE_IS_HIDDEN, isHidden());
     }
 }
